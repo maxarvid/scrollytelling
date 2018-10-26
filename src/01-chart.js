534b199392d3e9343644d67b93d9f343772cf032
Max <max.arvid.anderson@gmail.com>
2018-10-26 11:35:47 -0400

    added chart 2, tweaks to 1

--- src/01-chart.js
+++ src/01-chart.js
@@ -16,7 +16,7 @@ const svg = d3
   .attr('width', width + margin.left + margin.right)
   .attr('height', height + margin.top + margin.bottom)
   .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 const xPositionScale = d3.scaleBand().range([0, width])
 
--- src/02-chart.js
+++ src/02-chart.js
@@ -12,7 +12,7 @@ let svg = d3
   .attr('height', height + margin.top + margin.bottom)
   .attr('width', width + margin.left + margin.right)
   .append('g')
-  .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 let parseTime = d3.timeParse('%B-%y')
 
@@ -67,55 +67,8 @@ function ready(datapoints) {
     .entries(datapoints)
 
   svg
-    .selectAll('path')
-    .data(nested)
-    .enter()
-    .append('path')
-    .attr('d', function(d) {
-      return line(d.values)
-    })
-    .attr('stroke', function(d) {
-      return colorScale(d.key)
-    })
-    .attr('stroke-width', 2)
-    .attr('fill', 'none')
-
-  svg
-    .selectAll('circle')
-    .data(nested)
-    .enter()
-    .append('circle')
-    .attr('fill', function(d) {
-      return colorScale(d.key)
-    })
-    .attr('r', 4)
-    .attr('cy', function(d) {
-      return yPositionScale(d.values[0].price)
-    })
-    .attr('cx', function(d) {
-      return xPositionScale(d.values[0].datetime)
-    })
-
-  svg
-    .selectAll('text')
-    .data(nested)
-    .enter()
     .append('text')
-    .attr('y', function(d) {
-      return yPositionScale(d.values[0].price)
-    })
-    .attr('x', function(d) {
-      return xPositionScale(d.values[0].datetime)
-    })
-    .text(function(d) {
-      return d.key
-    })
-    .attr('dx', 6)
-    .attr('dy', 4)
-    .attr('font-size', '12')
-
-  svg
-    .append('text')
+    .attr('class', 'title-text')
     .attr('font-size', '24')
     .attr('text-anchor', 'middle')
     .text('U.S. housing prices fall in winter')
@@ -126,15 +79,6 @@ function ready(datapoints) {
   let rectWidth =
     xPositionScale(parseTime('February-17')) -
     xPositionScale(parseTime('November-16'))
-
-  svg
-    .append('rect')
-    .attr('x', xPositionScale(parseTime('December-16')))
-    .attr('y', 0)
-    .attr('width', rectWidth)
-    .attr('height', height)
-    .attr('fill', '#C2DFFF')
-    .lower()
 
   let xAxis = d3
     .axisBottom(xPositionScale)
@@ -151,4 +95,180 @@ function ready(datapoints) {
     .append('g')
     .attr('class', 'axis y-axis')
     .call(yAxis)
+
+  d3.select('#intro-graph-2').on('stepin', () => {
+    // console.log('hello intro graph 2!')
+    svg
+      .selectAll('.price-line')
+      .transition()
+      .remove()
+    svg
+      .selectAll('.price-circle')
+      .transition()
+      .remove()
+    svg
+      .selectAll('.region-text')
+      .transition()
+      .remove()
+  })
+
+  d3.select('#add-lines').on('stepin', () => {
+    // console.log(nested)
+    svg
+      .selectAll('.price-line')
+      .data(nested)
+      .enter()
+      .append('path')
+      .transition()
+      .attr('class', 'price-line')
+      .attr('d', d => line(d.values))
+      .attr('stroke', d => colorScale(d.key))
+      .attr('stroke-width', 2)
+      .attr('fill', 'none')
+
+    svg
+      .selectAll('.price-circle')
+      .data(nested)
+      .enter()
+      .append('circle')
+      .attr('class', 'price-circle')
+      .attr('r', 4)
+      .attr('cy', d => yPositionScale(d.values[0].price))
+      .attr('cx', d => xPositionScale(d.values[0].datetime))
+      .attr('fill', d => colorScale(d.key))
+
+    svg
+      .selectAll('.region-text')
+      .data(nested)
+      .enter()
+      .append('text')
+      .attr('class', 'region-text')
+      .attr('y', d => yPositionScale(d.values[0].price))
+      .attr('x', d => xPositionScale(d.values[0].datetime))
+      .text(d => d.key)
+      .attr('dx', 6)
+      .attr('dy', 4)
+      .attr('font-size', '12')
+  })
+
+  d3.select('#highlight-average').on('stepin', () => {
+    // console.log('avg highlight working')
+
+    svg.selectAll('.price-circle').attr('fill', d => {
+      if (d.key === 'U.S.') {
+        return 'red'
+      } else {
+        return 'lightgrey'
+      }
+    })
+
+    svg
+      .selectAll('.region-text')
+      .attr('fill', d => {
+        if (d.key === 'U.S.') {
+          return 'red'
+        } else {
+          return 'lightgrey'
+        }
+      })
+      .style('font-weight', 800)
+
+    svg.selectAll('.price-line').attr('stroke', d => {
+      if (d.key === 'U.S.') {
+        return 'red'
+      } else {
+        return 'lightgrey'
+      }
+    })
+  })
+
+  d3.select('#highlight-region').on('stepin', () => {
+    // console.log('region highlight working')
+    var expensiveRegions = [
+      'Pacific',
+      'Mountain',
+      'West South Central',
+      'South Atlantic'
+    ]
+    svg.selectAll('.price-circle').attr('fill', d => {
+      if (expensiveRegions.indexOf(d.key) !== -1) {
+        return '#c0e1eb'
+      } else if (d.key === 'U.S.') {
+        return 'red'
+      } else {
+        return 'lightgrey'
+      }
+    })
+
+    svg.selectAll('.region-text').attr('fill', d => {
+      if (expensiveRegions.indexOf(d.key) !== -1) {
+        return '#c0e1eb'
+      } else if (d.key === 'U.S.') {
+        return 'red'
+      } else {
+        return 'lightgrey'
+      }
+    })
+
+    svg.selectAll('.price-line').attr('stroke', d => {
+      if (expensiveRegions.indexOf(d.key) !== -1) {
+        return '#c0e1eb'
+      } else if (d.key === 'U.S.') {
+        return 'red'
+      } else {
+        return 'lightgrey'
+      }
+    })
+
+    svg
+      .select('.winter-rect')
+      .transition()
+      .remove()
+  })
+
+  d3.select('#winter-rect').on('stepin', () => {
+    // console.log('last step')
+    svg
+      .append('rect')
+      .attr('class', 'winter-rect')
+      .attr('x', xPositionScale(parseTime('December-16')))
+      .attr('y', 0)
+      .attr('width', rectWidth)
+      .attr('height', height)
+      .attr('fill', '#C2DFFF')
+      .lower()
+  })
+
+  function render() {
+    // console.log('time to rerender')
+    let screenWidth = svg.node().parentNode.parentNode.offsetWidth
+    let screenHeight = svg.node().parentNode.parentNode.offsetHeight
+    let newWidth = screenWidth - margin.left - margin.right
+    let newHeight = screenHeight - margin.top - margin.bottom
+
+    let actualSvg = d3.select(svg.node().parentNode)
+    actualSvg
+      .attr('height', newHeight + margin.top + margin.bottom)
+      .attr('width', newWidth + margin.left + margin.right)
+
+    xPositionScale.range([0, newWidth])
+    yPositionScale.range([newHeight, 0])
+
+    svg.select('.title-text').attr('x', newWidth / 2)
+
+    rectWidth =
+      xPositionScale(parseTime('February-17')) -
+      xPositionScale(parseTime('November-16'))
+
+    svg
+      .select('.winter-rect')
+      .attr('x', xPositionScale(parseTime('December-16')))
+      .attr('width', rectWidth)
+      .attr('height', newHeight)
+
+    svg.select('.x-axis').call(xAxis)
+    svg.select('.y-axis').call(yAxis)
+  }
+  window.addEventListener('resize', render)
+  render()
 }
